00401f1b4aadbdf2026a3ba3cc4b90ceec175730
rmdelapaz <raydelapaz@gmail.com>
2024-10-02 20:07:24 -0700

    Changes to be committed: 	modified:   demo.js 	new file:   linked_list_v1.html new file:   linked_list_v2.html

--- demo.js
+++ demo.js
@@ -1,56 +1,8 @@
-// let myArr = ["circle", "yellow", "cherry"]
+test();
+
+function test() {
+  console.log(hoistedVar); // undefined
+    var hoistedVar = 10;
+}
 
 
-// function sayHello() {
-//   let greeting = "hello"
-
-//   console.log(greeting);
-
-//   return greeting
-// }
-
-
-
-
-
-
-// let myObj = {}
-
-//   for (let key in myObj) {
-//     console.log(key + ": " + myObj[key]);
-//   }
-
-// function sayHello() {
-//   let greeting = "hello"
-
-//   console.log(greeting);
-
-//   return greeting
-// }
-
-// let expHello =  () => {
-//   let greeting = "hello"
-
-//   console.table(greeting);
-
-//   return greeting
-// }
-
-// function countWords(words) {
-//   const counter = {};
-
-//   for (let word of words) {
-//       if (counter[word] !== undefined) {
-//           counter[word]++;
-//       } else {
-//           counter[word] = 1;
-//       }
-//   }
-
-//   return counter;
-// }
-const person = {
-  name: 'Alice',
-  age: 25,
-  city: 'Wonderland'
-};
